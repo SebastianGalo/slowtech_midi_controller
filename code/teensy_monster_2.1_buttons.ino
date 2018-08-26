8ba461a16c5f82e8f91ca8bb2e9e890dd6816ef4
Imbecillen <yo@sebastiangalo.se>
2018-08-26 12:21:16 +0200

    Update teensy_monster_2.1_buttons.ino

--- code/teensy_monster_2.1_buttons.ino
+++ code/teensy_monster_2.1_buttons.ino
@@ -6,7 +6,7 @@
 // Edit: Sebastian Galo 2018
 
 // Use name.c as tab in the sketch to show custom midi name
-// Distilled code for the project tested width 16 buttons, communication working fine. 
+// Distilled code for the project tested with 16 buttons, communication working fine. 
 // Maybe chashing due to midi overflow?
 // Button mode 4 sending messages directly when pressed instead of released.
 
