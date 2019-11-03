fca0e3eed933dfde8c38db2d44f5122fbc30e145
Imbecillen <yo@sebastiangalo.se>
2019-11-03 21:39:51 +0100

    Add files via upload

---
+++ code/slowtech_3.0.ino
@@ -0,0 +1,20 @@
+
+
+
+// This project is based on the Teensy Monster 1.0: https://github.com/FuzzyWobble/TeensyMonster
+// License: http://opensource.org/licenses/MIT
+// Edit: Imbecillen / Sebastian Galo 2018
+
+
+#include <SPI.h>  // SPI library
+#include <Wire.h> // I2C library
+#include <Adafruit_GFX.h> // Display graphics library
+#include <Adafruit_SSD1306.h> // Display library
+#include <BitBool.h> //?
+
+#define  OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+
+
+
